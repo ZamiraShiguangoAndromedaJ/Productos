4c07598a89ae076fcb763305765df08be649cedd
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2025-02-13 13:26:45 -0500

    Creando repositorio Gestor producto

---
+++ src/main/java/espcoh/ec/edu/gestorproducto/GestorProducto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espcoh.ec.edu.gestorproducto;
+
+/**
+ *
+ * @author user
+ */
+public class GestorProducto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
